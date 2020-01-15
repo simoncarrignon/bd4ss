9424ecab096267d981c85e93a62823e93ed45f24
Simon Carrignon <simon@nimbios.org>
2020-01-15 13:41:39 -0500

    update building script

--- _build.sh
+++ _build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-set -ev
-
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+Rscript -e "bookdown::render_book('index.Rmd')"
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
